4b21cd3da4f69c84ccce8f0cf7ac03f92747fa7b
Aarav Parikh <aaravparikh77@gmail.com>
2021-09-11 10:30:01 +0530

    password reset changes

--- includes/reset.inc.php
+++ includes/reset.inc.php
@@ -2,22 +2,34 @@
 
 if(isset($_POST["submit"])){
 
-    $email = "pankhudi.mohan@jnis.ac.in";
+    $email = $_POST["email"];
     $pwd = $_POST["pwd"];
     $pwdRepeat = $_POST["rePwd"];
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
 
-    if(emptyInputSignup($email, $pwd, $pwdRepeat) !== false){
-        header("location: ../passwordReset.php?error=emptyInput");
+    if(emptyInputSignup($email,$pwd,$pwdRepeat, $reEmail) !== false){
+        header("location: ../register.php?error=emptyInput");
         exit();
     }
 
+    else if(invalidEmail($email) !== false){
+        header("location: ../register.php?error=invalidEmail");
+        exit();
+    }
 
+    else if(pwdMatch($email, $reEmail) !== false){
+        header("location:../register.php?error=emailMatchError");
+    }
 
     else if(pwdMatch($pwd, $pwdRepeat) !== false){
-        header("location: ../passwordReset.php?error=pwdMatchError");
+        header("location: ../register.php?error=pwdMatchError");
+        exit();
+    }
+
+    else if(emailExists($conn, $email) !== true){
+        header("location: ../register.php?error=nonExistentUser");
         exit();
     }
 
